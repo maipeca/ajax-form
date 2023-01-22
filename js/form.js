eda776c6409d2f3f694affbfe585d4fe6af0ff75
Maite <maipeca@hotmail.com>
2023-01-22 17:22:15 +0100

    creating a form with ajax

---
+++ js/form.js
@@ -0,0 +1,122 @@
+
+const form = document.querySelector('#form');
+const nome = document.getElementById('nome');
+const cpf = document.getElementById('cpf');
+const email = document.getElementById('email');
+const cep = document.getElementById('cep');
+const bairro = document.getElementById('bairro');
+const cidade = document.getElementById('cidade');
+const uf = document.getElementById('uf');
+const rua = document.getElementById('rua');
+const mensagem = document.querySelector('#mensagem');
+const notNull = document.getElementsByClassName('not-null');
+
+///////////////NAME
+function isEmpty(elem){
+    return elem.value.length < 1 ? `O campo <strong>${elem.name}</strong> não pode ser vazio` : ''; 
+}
+
+//////////////EMAIL
+function validaEmail(elem){
+    return elem.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? '' : `Digite um <strong>e-mail</strong> válido`;
+}
+
+////////////////CEP
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+  
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById('rua').value=(conteudo.logradouro);
+    document.getElementById('bairro').value=(conteudo.bairro);
+    document.getElementById('cidade').value=(conteudo.localidade);
+    document.getElementById('uf').value=(conteudo.uf);
+    
+} //end if.
+else {
+    //CEP não Encontrado.
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+}
+}
+
+function pesquisacep(valor) {
+
+//Nova variável "cep" somente com dígitos.
+var cep = valor.replace(/\D/g, '');
+
+//Verifica se campo cep possui valor informado.
+if (cep != "") {
+
+    //Expressão regular para validar o CEP.
+    var validacep = /^[0-9]{8}$/;
+
+    //Valida o formato do CEP.
+    if(validacep.test(cep)) {
+
+        //Preenche os campos com "..." enquanto consulta webservice.
+        document.getElementById('rua').value="...";
+        document.getElementById('bairro').value="...";
+        document.getElementById('cidade').value="...";
+        document.getElementById('uf').value="...";
+       
+
+        //Cria um elemento javascript.
+        var script = document.createElement('script');
+
+        //Sincroniza com o callback.
+        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+        //Insere script no documento e carrega o conteúdo.
+        document.body.appendChild(script);
+
+    } //end if.
+    else {
+        //cep é inválido.
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+    }
+} //end if.
+else {
+    //cep sem valor, limpa formulário.
+    limpa_formulário_cep();
+}
+};
+////////////////////////////////////////////
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+
+    let msg = [];
+    let markup = '';
+   
+    Array.from(notNull).forEach(field => {
+        let fieldState = isEmpty(field);
+        if(fieldState) 
+            msg.push(fieldState);
+    });
+
+    const isEmail = validaEmail(email);
+    if(isEmail) msg.push(isEmail);
+
+    msg.forEach(item => {
+        markup += `<p>${item}</p>` 
+    });
+
+
+
+    
+
+    mensagem.innerHTML = markup;
+
+     //if(msg.length == 0)  form.submit();
+
+});
